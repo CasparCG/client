e48fd545a56d6bed493982e145da4cd8d128efc3
nirklas <nirklas@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-06-07 18:32:17 +0000

    Makes the project build again!
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/clients/csharp/Rundown/trunk@3108 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

---
+++ CasparCG.Client/CGDataListWrapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Caspar_Pilot
+{
+    class CGDataListWrapper : Svt.Caspar.ICGDataContainer
+    {
+        private List<Svt.Caspar.CGDataPair> Data { get; set; }
+        public CGDataListWrapper(List<Svt.Caspar.CGDataPair> list)
+        {
+            Data = list;
+        }
+
+        public string ToAMCPEscapedXml()
+        {
+            return Svt.Caspar.CGDataPair.ToEscapedXml(Data);
+        }
+
+        public string ToXml()
+        {
+            return Svt.Caspar.CGDataPair.ToXml(Data);
+        }
+    }
+}
