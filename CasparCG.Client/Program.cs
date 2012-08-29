b5ad95f59bc7efd4788dee3e4ed594c1562d9aa6
Zebiolo <Zebiolo@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2012-08-29 10:59:07 +0000

    Catch COM-exception if flash player is not installed.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/clients/csharp/Rundown/trunk@3220 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- CasparCG.Client/Program.cs
+++ CasparCG.Client/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
 
 namespace Caspar_Pilot
 {
@@ -12,9 +14,16 @@ namespace Caspar_Pilot
 		[STAThread]
 		static void Main()
 		{
-			Application.EnableVisualStyles();
-			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new OperatorForm());
+            try
+            {
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                Application.Run(new OperatorForm());
+            }
+            catch (COMException ex)
+            {
+                MessageBox.Show("An error occurred when loading flash player. Do you have flash player installed?", "CasparCG Client", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
 		}
 	}
 }
