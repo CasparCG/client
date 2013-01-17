4238211f8f0c7cc1e02d8faf1b4c9d55785ef86d
zebiolo <zebiolo@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2013-01-17 13:21:39 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/clients/cpp/Client/trunk@3677 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- Widgets/Rundown/RundownWidget.cpp
+++ Widgets/Rundown/RundownWidget.cpp
@@ -151,7 +151,9 @@ bool RundownWidget::eventFilter(QObject* target, QEvent* event)
             return executeCommand(Playout::PlayoutType::ClearChannel, KeyPress);
 
         if (target == treeWidgetRundown)
-        {  
+        {
+            qDebug() << keyEvent->modifiers();
+            qDebug() <<  keyEvent->key();
             if (keyEvent->key() == Qt::Key_Delete)
                 return removeSelectedItems();
             else if (keyEvent->key() == Qt::Key_C && keyEvent->modifiers() == Qt::ControlModifier)
@@ -162,7 +164,7 @@ bool RundownWidget::eventFilter(QObject* target, QEvent* event)
                 return groupItems();
             else if (keyEvent->key() == Qt::Key_U && keyEvent->modifiers() == Qt::ControlModifier)
                 return ungroupItems();
-            else if (keyEvent->key() == Qt::Key_Up && keyEvent->modifiers() == Qt::ControlModifier)
+            else if (keyEvent->key() == Qt::Key_Up && (keyEvent->modifiers() == Qt::ControlModifier || (keyEvent->modifiers() == Qt::ControlModifier && keyEvent->modifiers() == Qt::KeyboardModifier)))
                 return moveItemUp();
             else if (keyEvent->key() == Qt::Key_Down && keyEvent->modifiers() == Qt::ControlModifier)
                 return moveItemDown();
