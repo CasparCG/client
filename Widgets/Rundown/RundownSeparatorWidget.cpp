2590bc60331470dfbd4599b90660f11b2b64f1eb
Zebiolo <Zebiolo@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2013-01-28 08:00:27 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/clients/cpp/Client/trunk@3715 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

---
+++ Widgets/Rundown/RundownSeparatorWidget.cpp
@@ -0,0 +1,116 @@
+#include "RundownSeparatorWidget.h"
+
+#include "Global.h"
+#include "GpiManager.h"
+
+#include "Events/RundownItemChangedEvent.h"
+
+#include <QtGui/QApplication>
+#include <QtCore/QObject>
+
+RundownSeparatorWidget::RundownSeparatorWidget(const LibraryModel& model, QWidget* parent, const QString& color,
+                                               bool active, bool inGroup, bool compactView)
+    : QWidget(parent),
+      active(active), inGroup(inGroup), compactView(compactView), color(color), model(model)
+{
+    setupUi(this);
+    setColor(color);
+    setActive(active);
+    setCompactView(compactView);
+
+    this->labelGroupColor->setVisible(this->inGroup);
+    this->labelGroupColor->setStyleSheet(QString("background-color: %1;").arg(Color::DEFAULT_GROUP_COLOR));
+    this->labelColor->setStyleSheet(QString("background-color: %1;").arg(Color::DEFAULT_MEDIA_COLOR));
+
+    this->labelLabel->setText(this->model.getLabel());
+
+    qApp->installEventFilter(this);
+}
+
+bool RundownSeparatorWidget::eventFilter(QObject* target, QEvent* event)
+{
+    if (event->type() == static_cast<QEvent::Type>(Enum::EventType::RundownItemChanged))
+    {
+        // This event is not for us.
+        if (!this->active)
+            return false;
+
+        RundownItemChangedEvent* rundownItemChangedEvent = dynamic_cast<RundownItemChangedEvent*>(event);
+        this->model.setLabel(rundownItemChangedEvent->getLabel());
+        this->model.setName(rundownItemChangedEvent->getName());
+
+        this->labelLabel->setText(this->model.getLabel());
+    }
+
+    return QObject::eventFilter(target, event);
+}
+
+IRundownWidget* RundownSeparatorWidget::clone()
+{
+    RundownSeparatorWidget* widget = new RundownSeparatorWidget(this->model, this->parentWidget(), this->color,
+                                                                this->active, this->inGroup, this->compactView);
+
+    return widget;
+}
+
+void RundownSeparatorWidget::readProperties(boost::property_tree::wptree& pt)
+{
+    if (pt.count(L"color") > 0) setColor(QString::fromStdWString(pt.get<std::wstring>(L"color")));
+}
+
+void RundownSeparatorWidget::writeProperties(QXmlStreamWriter* writer)
+{
+    writer->writeTextElement("color", this->color);
+}
+
+void RundownSeparatorWidget::setCompactView(bool compactView)
+{
+    if (compactView)
+        this->labelThumbnail->setFixedSize(Define::COMPACT_VIEW_WIDTH, Define::COMPACT_VIEW_HEIGHT);
+    else
+        this->labelThumbnail->setFixedSize(Define::DEFAULT_VIEW_WIDTH, Define::DEFAULT_VIEW_HEIGHT);
+
+    this->compactView = compactView;
+}
+
+bool RundownSeparatorWidget::isGroup() const
+{
+    return false;
+}
+
+ICommand* RundownSeparatorWidget::getCommand()
+{
+    return &this->command;
+}
+
+LibraryModel* RundownSeparatorWidget::getLibraryModel()
+{
+    return &this->model;
+}
+
+void RundownSeparatorWidget::setColor(const QString& color)
+{
+    this->color = color;
+    this->setStyleSheet(QString("#frameItem, #frameStatus { background-color: rgba(%1); }").arg(color));
+}
+
+void RundownSeparatorWidget::setActive(bool active)
+{
+    this->active = active;
+
+    if (this->active)
+        this->labelActiveColor->setStyleSheet("background-color: red;");
+    else
+        this->labelActiveColor->setStyleSheet("");
+}
+
+void RundownSeparatorWidget::setInGroup(bool inGroup)
+{
+    this->inGroup = inGroup;
+    this->labelGroupColor->setVisible(inGroup);
+}
+
+bool RundownSeparatorWidget::executeCommand(enum Playout::PlayoutType::Type type)
+{
+    return false;
+}
---
+++ Widgets/Rundown/RundownSeparatorWidget.h
@@ -0,0 +1,59 @@
+#pragma once
+
+#include "../Shared.h"
+#include "IPlayoutCommand.h"
+#include "IRundownWidget.h"
+#include "ui_RundownSeparatorWidget.h"
+
+#include "Global.h"
+
+#include "Commands/ICommand.h"
+#include "Commands/SeparatorCommand.h"
+#include "Models/LibraryModel.h"
+
+#include <stdexcept>
+
+#include <QtCore/QString>
+
+#include <QtGui/QWidget>
+
+class WIDGETS_EXPORT RundownSeparatorWidget : public QWidget, Ui::RundownSeparatorWidget, public IRundownWidget, public IPlayoutCommand
+{
+    Q_OBJECT
+
+    public:
+        explicit RundownSeparatorWidget(const LibraryModel& model, QWidget* parent = 0,
+                                        const QString& color = Color::DEFAULT_TRANSPARENT_COLOR,
+                                        bool active = false, bool inGroup = false, bool compactView = false);
+
+        virtual IRundownWidget* clone();
+
+        virtual bool isGroup() const;
+
+        virtual ICommand* getCommand();
+        virtual LibraryModel* getLibraryModel();
+
+        virtual void setActive(bool active);
+        virtual void setInGroup(bool inGroup);
+        virtual void setColor(const QString& color);
+
+        virtual void setExpanded(bool expanded) {}
+
+        virtual bool executeCommand(enum Playout::PlayoutType::Type type);
+
+        virtual void readProperties(boost::property_tree::wptree& pt);
+        virtual void writeProperties(QXmlStreamWriter* writer);
+
+        virtual void setCompactView(bool compactView);
+
+    protected:
+        virtual bool eventFilter(QObject* target, QEvent* event);
+
+    private:
+        bool active;
+        bool inGroup;
+        bool compactView;
+        QString color;
+        LibraryModel model;
+        SeparatorCommand command;
+};
