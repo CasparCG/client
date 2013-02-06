9d0aba6e6cf0141b7fe2732dbd9f1fb01646ab8a
Zebiolo <Zebiolo@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2013-02-06 20:32:41 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/clients/cpp/Client/trunk@3737 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- Core/Animations/AlphaAnimation.cpp
+++ Core/Animations/AlphaAnimation.cpp
@@ -1,19 +1,21 @@
 #include "AlphaAnimation.h"
+#include "QDebug"
 
 AlphaAnimation::AlphaAnimation(QWidget* target, QObject* parent)
     : QObject(parent),
     value(255), target(target), animation(NULL)
 {
     this->animation = new QPropertyAnimation(this, "alpha");
-    this->animation->setDuration(1000);
+    this->animation->setDuration(500);
     this->animation->setKeyValueAt(0, 0);
-    this->animation->setKeyValueAt(0.5, 192);
+    //this->animation->setKeyValueAt(0.5, 192); // 192
+    //this->animation->setKeyValueAt(0.6, 255); // 192
     this->animation->setKeyValueAt(1, 255);
 }
 
 void AlphaAnimation::start(int loopCount)
 {
-    this->animation->setLoopCount(loopCount);
+    //this->animation->setLoopCount(loopCount);
     this->animation->start();
 }
 
@@ -26,6 +28,7 @@ void AlphaAnimation::setAlpha(const int value)
 {
     this->value = value;
 
-    QString stylesheet(QString("color: rgba(255, 255, 255, %1);").arg(this->value));
+    qDebug() << QString("background-color: rgb(%1, %2, 0);").arg(255 - this->value).arg(this->value);
+    QString stylesheet(QString("background-color: rgb(%1, %2, 0);").arg(255 - this->value).arg(this->value));
     this->target->setStyleSheet(stylesheet);
 }
