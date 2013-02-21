12984659a31a032863714a133db7cf19f77dc946
Zebiolo <Zebiolo@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2013-02-21 13:12:24 +0000

    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/clients/cpp/Client/trunk@3758 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- Core/Commands/OpacityCommand.cpp
+++ Core/Commands/OpacityCommand.cpp
@@ -60,7 +60,7 @@ void OpacityCommand::readProperties(boost::property_tree::wptree& pt)
     if (pt.count(L"opacity") > 0) setOpacity(pt.get<float>(L"opacity"));
     if (pt.count(L"duration") > 0) setDuration(pt.get<int>(L"duration"));
     if (pt.count(L"tween") > 0) setTween(QString::fromStdWString(pt.get<std::wstring>(L"tween")));
-    if (pt.count(L"tween") > 0) setDefer(pt.get<bool>(L"tween"));
+    if (pt.count(L"defer") > 0) setDefer(pt.get<bool>(L"defer"));
 }
 
 void OpacityCommand::writeProperties(QXmlStreamWriter* writer)
