ab6cb53d194cf863bff94c2156ef4f0ae89f4f17
Zebiolo <Zebiolo@362d55ac-95cf-4e76-9f9a-cbaa9c17b72d>
2013-03-26 14:10:54 +0000

    - UI changes.
    git-svn-id: https://casparcg.svn.sourceforge.net/svnroot/casparcg/clients/cpp/Client/trunk@3789 362d55ac-95cf-4e76-9f9a-cbaa9c17b72d

--- Widgets/MainWindow.cpp
+++ Widgets/MainWindow.cpp
@@ -27,6 +27,7 @@ MainWindow::MainWindow(QWidget* parent) : QMainWindow(parent)
     setupUiMenu();
     setWindowIcon(QIcon(":/Graphics/Images/CasparCG.png"));
 
+
     setWindowTitle(QString("%1 %2.%3 %4").arg(this->windowTitle()).arg(MAJOR_VERSION).arg(MINOR_VERSION).arg(TAG_VERSION));
     this->applicationTitle = this->windowTitle();
 
