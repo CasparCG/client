165803bdf386d99dacba5c1ff0bce83e3d4ec778
Helge Norberg <helge.norberg@svt.se>
2014-09-30 10:01:57 +0200

    Created build/release scripts for windows

---
+++ build-scripts/set-variables-and-build-windows.bat
@@ -0,0 +1,9 @@
+@echo off
+
+set BUILD_QMAKE=C:\Qt\4.8.1\bin\qmake.exe
+set BUILD_MINGW_BIN=C:\Qt\qtcreator-2.4.1\mingw\bin
+set BUILD_CLIENT_NAME=CasparCG Client 207
+set BUILD_PARALLEL_THREADS=12
+set BUILD_7ZIP=C:\Program Files\7-Zip\7z.exe
+
+build-windows.bat
