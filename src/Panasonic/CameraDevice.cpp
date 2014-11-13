b1ebf48490f071b0c7049fa7330641c9663bcdaa
Peter Karlsson <peter.p.karlsson@svt.se>
2014-11-13 14:26:46 +0100

    Fixed bug with Panasonic camera presets.

--- src/Panasonic/CameraDevice.cpp
+++ src/Panasonic/CameraDevice.cpp
@@ -10,7 +10,7 @@ CameraDevice::CameraDevice(QObject* parent)
 
 void CameraDevice::selectPreset(const QString& address, const QString& preset)
 {
-    QUrl request(QString("http://%1/cgi-bin/aw_ptz?%2").arg(address).arg(preset));
+    QUrl request(QString("http://%1/cgi-bin/aw_ptz?cmd=#R%2&res=1").arg(address).arg(preset));
 
     qDebug() << QString("CameraDevice::selectPreset: %1").arg(request.toString());
 
