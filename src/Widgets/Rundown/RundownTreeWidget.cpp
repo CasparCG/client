164b07d1e00e8d75aed3033101137bae3aa2fcd1
Peter Karlsson <peter.p.karlsson@svt.se>
2015-02-17 11:16:11 +0100

    Fixed crach bug when reloading iNews rundowns.

--- src/Widgets/Rundown/RundownTreeWidget.cpp
+++ src/Widgets/Rundown/RundownTreeWidget.cpp
@@ -707,15 +707,20 @@ void RundownTreeWidget::reloadRundown()
     if (this->activeRundown == Rundown::DEFAULT_NAME)
         return;
 
-    delete this->copyItem;
-    delete this->currentAutoPlayWidget;
-    delete this->currentPlayingItem;
-    delete this->currentPlayingAutoStepItem;
+    if (this->copyItem != NULL)
+        this->copyItem = NULL;
 
-    this->copyItem = NULL;
-    this->currentAutoPlayWidget = NULL;
-    this->currentPlayingItem = NULL;
-    this->currentPlayingAutoStepItem = NULL;
+    if (this->currentPlayingItem != NULL)
+        this->currentPlayingItem = NULL;
+
+    if (this->currentPlayingAutoStepItem != NULL)
+        this->currentPlayingAutoStepItem = NULL;
+
+    if (this->currentAutoPlayWidget != NULL)
+    {
+        delete this->currentAutoPlayWidget;
+        this->currentAutoPlayWidget = NULL;
+    }
 
     this->treeWidgetRundown->removeAllItems();
 
