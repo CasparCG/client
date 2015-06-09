766b869d1b5d21278d238155e57d20257e808049
Peter Karlsson <peter.p.karlsson@svt.se>
2015-06-09 16:14:32 +0200

    Added delete statements in the sql change script.

--- src/Core/Sql/ChangeScript-208.sql
+++ src/Core/Sql/ChangeScript-208.sql
@@ -5,3 +5,8 @@ INSERT INTO AtemMixerStep (Name, Value) VALUES('1', '0');
 INSERT INTO AtemMixerStep (Name, Value) VALUES('2', '1');
 
 ALTER TABLE Device ADD COLUMN LockedChannel INTEGER DEFAULT 0;
+
+DELETE FROM Chroma WHERE Key = 'Red'
+DELETE FROM Chroma WHERE Key = 'Yellow'
+DELETE FROM Chroma WHERE Key = 'Torquise'
+DELETE FROM Chroma WHERE Key = 'Magenta'
