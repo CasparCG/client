c9f75dff59dfb01b3896c681cf4bacd3b3ed07a8
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2015-09-13 15:01:54 -0400

    RundownFadeToBlackWidget: show arrow down

--- src/Widgets/Rundown/RundownFadeToBlackWidget.cpp
+++ src/Widgets/Rundown/RundownFadeToBlackWidget.cpp
@@ -52,6 +52,7 @@ RundownFadeToBlackWidget::RundownFadeToBlackWidget(const LibraryModel& model, QW
     QObject::connect(&this->command, SIGNAL(videolayerChanged(int)), this, SLOT(videolayerChanged(int)));
     QObject::connect(&this->command, SIGNAL(delayChanged(int)), this, SLOT(delayChanged(int)));
     QObject::connect(&this->command, SIGNAL(allowGpiChanged(bool)), this, SLOT(allowGpiChanged(bool)));
+    QObject::connect(&this->command, SIGNAL(autoStepChanged(bool)), this, SLOT(autoStepChanged(bool)));
     QObject::connect(&this->command, SIGNAL(remoteTriggerIdChanged(const QString&)), this, SLOT(remoteTriggerIdChanged(const QString&)));
     QObject::connect(&EventManager::getInstance(), SIGNAL(deviceChanged(const DeviceChangedEvent&)), this, SLOT(deviceChanged(const DeviceChangedEvent&)));
     QObject::connect(&EventManager::getInstance(), SIGNAL(labelChanged(const LabelChangedEvent&)), this, SLOT(labelChanged(const LabelChangedEvent&)));
@@ -610,6 +611,11 @@ void RundownFadeToBlackWidget::allowGpiChanged(bool allowGpi)
     Q_UNUSED(allowGpi);
 
     checkGpiConnection();
+}
+
+void RundownFadeToBlackWidget::autoStepChanged(bool autoStep)
+{
+    this->labelAutoStep->setVisible(autoStep);
 }
 
 void RundownFadeToBlackWidget::gpiConnectionStateChanged(bool connected, GpiDevice* device)
