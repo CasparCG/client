612d789e56eb9dcfdddd97492783526f2d96314b
Dimitry Ishenko <dimitry.ishenko@gmail.com>
2016-05-20 11:47:03 -0400

    SaturationCommand: fix typo in transitionDuration (extra "t")
    For now, support reading "transtitionChanged" from the XML file, to
    avoid breaking existing rundowns.
    Users should be advised to open and save their rundowns to fix the typo.

--- src/Core/Commands/SaturationCommand.cpp
+++ src/Core/Commands/SaturationCommand.cpp
@@ -14,7 +14,7 @@ float SaturationCommand::getSaturation() const
 
 int SaturationCommand::getTransitionDuration() const
 {
-    return this->transtitionDuration;
+    return this->transitionDuration;
 }
 
 const QString& SaturationCommand::getTween() const
@@ -33,10 +33,10 @@ void SaturationCommand::setSaturation(float saturation)
     emit saturationChanged(this->saturation);
 }
 
-void SaturationCommand::setTransitionDuration(int transtitionDuration)
+void SaturationCommand::setTransitionDuration(int transitionDuration)
 {
-    this->transtitionDuration = transtitionDuration;
-    emit transtitionDurationChanged(this->transtitionDuration);
+    this->transitionDuration = transitionDuration;
+    emit transitionDurationChanged(this->transitionDuration);
 }
 
 void SaturationCommand::setTween(const QString& tween)
@@ -56,7 +56,7 @@ void SaturationCommand::readProperties(boost::property_tree::wptree& pt)
     AbstractCommand::readProperties(pt);
 
     setSaturation(pt.get(L"saturation", Mixer::DEFAULT_SATURATION));
-    setTransitionDuration(pt.get(L"transtitionDuration", Mixer::DEFAULT_DURATION));
+    setTransitionDuration(pt.get(L"transitionDuration", pt.get(L"transtitionDuration", Mixer::DEFAULT_DURATION)));
     setTween(QString::fromStdWString(pt.get(L"tween", Mixer::DEFAULT_TWEEN.toStdWString())));
     setDefer(pt.get(L"defer", Mixer::DEFAULT_DEFER));
 }
@@ -66,7 +66,7 @@ void SaturationCommand::writeProperties(QXmlStreamWriter* writer)
     AbstractCommand::writeProperties(writer);
 
     writer->writeTextElement("saturation", QString::number(getSaturation()));
-    writer->writeTextElement("transtitionDuration", QString::number(getTransitionDuration()));
+    writer->writeTextElement("transitionDuration", QString::number(getTransitionDuration()));
     writer->writeTextElement("tween", getTween());
     writer->writeTextElement("defer", (getDefer() == true) ? "true" : "false");
 }
