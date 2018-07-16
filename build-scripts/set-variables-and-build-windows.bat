c588601564703795354238fbd88dde7977574441
dedicatedbroadcastsolutions <zcybercomputing@gmail.com>
2018-07-15 23:35:24 -0700

    Revert "Update build script"
    This reverts commit 077aa71b4d5a2c04f5ae68340de80fd1e15f3043.

--- build-scripts/set-variables-and-build-windows.bat
+++ build-scripts/set-variables-and-build-windows.bat
@@ -1,9 +1,9 @@
 @echo off
 
-set BUILD_QMAKE=C:\Qt\qt-5.4.0-x64-mingw492r0-seh-opengl\qt-5.4.0-x64-mingw492r0-seh-opengl\bin\qmake.exe
-set BUILD_MINGW_BIN=C:\Qt\qt-5.4.0-x64-mingw492r0-seh-opengl\mingw64\bin
+set BUILD_QMAKE=C:\Qt\4.8.5\bin\qmake.exe
+set BUILD_MINGW_BIN=C:\Qt\qtcreator-2.4.1\mingw\bin
 set BUILD_ARCHIVE_NAME=CasparCG Client
-set BUILD_PARALLEL_THREADS=20
+set BUILD_PARALLEL_THREADS=8
 set BUILD_7ZIP=C:\Program Files\7-Zip\7z.exe
 
 build-windows.bat
