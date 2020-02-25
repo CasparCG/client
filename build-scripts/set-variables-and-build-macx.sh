bb1164617b952a1d65f69989e4f922a7c268cf16
Graham Spratt <37407811+grahamspr86@users.noreply.github.com>
2020-02-25 10:28:18 +0000

    Updated mac build script to use logicalcpu number

--- build-scripts/set-variables-and-build-macx.sh
+++ build-scripts/set-variables-and-build-macx.sh
@@ -2,7 +2,7 @@
 
 export BUILD_QT_PATH=~/Qt/Qt-5.4.1/5.4/clang_64/bin
 export BUILD_ARCHIVE_NAME="CasparCG Client"
-export BUILD_PARALLEL_THREADS=4
+export BUILD_PARALLEL_THREADS=$((`sysctl -n hw.logicalcpu`))
 export BUILD_HDIUTILS_WORKAROUND_SIZE="130m"
 
 ./build-macx.sh
