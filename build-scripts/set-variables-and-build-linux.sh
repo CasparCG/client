a31f877d6b5d09f6a9e6e9d4d4a6a937fe119791
Peter Karlsson <peter@zapto.se>
2020-05-02 13:23:02 +0200

    Fixed "qmake not found" issue

--- build-scripts/set-variables-and-build-linux.sh
+++ build-scripts/set-variables-and-build-linux.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-export BUILD_QT_PATH=~/Qt/Qt-5.14.2/5.14.2/gcc_64/bin
+export BUILD_QT_PATH=$HOME/Qt/Qt-5.14.2/5.14.2/gcc_64/bin
 export BUILD_ARCHIVE_NAME="CasparCG Client"
 export BUILD_PARALLEL_THREADS=8
 
