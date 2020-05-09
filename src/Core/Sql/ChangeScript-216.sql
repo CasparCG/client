816f89c82baaf421d37319535c093662870c7b54
Peter Karlsson <peter@zapto.se>
2020-05-09 11:44:08 +0200

    Fixed wrong database version in the schema and change set.

--- src/Core/Sql/ChangeScript-216.sql
+++ src/Core/Sql/ChangeScript-216.sql
@@ -1,1 +1,4 @@
 INSERT INTO OscOutput (Name, Address, Port, Description) VALUES('Localhost', '127.0.0.1', 3250, '');
+
+UPDATE Configuration SET Value = '3250' WHERE Name = 'OscControlPort';
+UPDATE Configuration SET Value = '6250' WHERE Name = 'OscMonitorPort';
